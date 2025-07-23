a6489c90589ccfae6e5d1af82eb377e15ad60d43
king-yummy <wingting2642@naver.com>
2025-07-23 16:20:53 +0900

    이벤트 문구 추가3

--- js/main.js
+++ js/main.js
@@ -1049,11 +1049,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
       const header = "🔥 PLLI 스밍/투표 독려 이벤트 🔥";
       const eventInfo =
-        "📅 기간: 7/21(월) ~ 8/4(월) 23:59\n" +
-        "🎁 상품: 8/5 추첨! 덕질 자금 1만원 (1명)\n" +
-        "💗 많이 공유할수록 당첨 확률 UP! UP!\n\n" +
-        "👇 로그인 필요없이 앱 하단에서 바로 참여 가능!";
-      const tags = "#PLLI_스밍투표_이벤트 #PLLI_화력응원 #PLAVE";
+        "📅 기간: 7/21(월) ~ 8/4(월)\n" +
+        "🎁 상품: 덕질 자금 1만원 (1명)\n" +
+        "💗 많이 공유할수록 당첨 확률 UP\n\n" +
+        "👇 회원가입 없이 앱 하단에서 바로 참여!";
+      const tags = "#PLLI_스밍투표_이벤트 #PLLI_화력응원";
       const url = "https://www.plli-checker.app";
 
       // 헤더, 메시지, 이벤트 정보, 태그, URL 사이에 줄바꿈(\n\n)을 추가하여 가독성을 확보합니다.
