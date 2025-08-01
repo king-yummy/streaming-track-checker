92e83e5a8f1fbec8561eae3509d73446a4a4597c
king-yummy <wingting2642@naver.com>
2025-08-02 01:38:15 +0900

    확장 테스트3

--- api/validate-password.js
+++ api/validate-password.js
@@ -1,24 +1,7 @@
-// /api/validate-password.js
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // 👈 모든 출처 허용 (or 제한 가능)
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow Chrome extension fetch
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
-  const validPassword = process.env.PLLI_PASSWORD;
-  const input = req.query.pw;
-
-  if (!validPassword) {
-    return res.status(500).json({ success: false, error: "No password set." });
-  }
-
-  if (input === validPassword) {
-    return res.status(200).json({ success: true });
-  } else {
-    return res.status(401).json({ success: false });
-  }
-}
-
-
-export default function handler(req, res) {
   const validPassword = process.env.PLLI_PASSWORD;
   const input = req.query.pw;
 
