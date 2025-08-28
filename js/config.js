850fcc9318664a0e6b1e64da838441128fc5454c
king-yummy <wingting2642@naver.com>
2025-08-28 13:33:39 +0900

    슈퍼팬2

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 // Google Sheets API 설정
 export const SPREADSHEET_ID = "1tGslp_8ahx8E5Y8kvIFq3DAcciLgtSyTvlTBROOrsKg";
 
+export const GOOGLE_SHEETS_API_KEY = "AIzaSyC1ChqA2Nc_ry82lMjsqPaR93B_FywoDOY";
+
 // 시트 제목 (탭 이름) — 실제 시트에 맞게 수정
 export const STREAMING_SHEET_TITLE = "StreamingList";
 export const TODO_SHEET_TITLE = "TodoList";
