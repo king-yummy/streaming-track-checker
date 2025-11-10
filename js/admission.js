8fe968f277984eb2e21e80c22d6dd9747392e53c
king-yummy <wingting2642@naver.com>
2025-11-10 18:29:05 +0900

    원클릭

--- js/admission.js
+++ js/admission.js
@@ -191,10 +191,11 @@ document.addEventListener("DOMContentLoaded", () => {
       </p>
 
       <div class="grid grid-cols-2 gap-3 mb-6">
-        <a href="https://tinyurl.com/4hkbdx8c" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">1번 링크</a>
-        <a href="https://tinyurl.com/2ft4u95u" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">2번 링크</a>
-        <a href="https://tinyurl.com/4s8w5ezd" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">3번 링크</a>
-        <a href="https://tinyurl.com/nhzj28kj" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">4번 링크</a>
+        <a href="https://tinyurl.com/yc8e2arj" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">1번 링크</a>
+<a href="https://tinyurl.com/cshwxxbm" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">2번 링크</a>
+<a href="https://tinyurl.com/4mfnxwfa" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">3번 링크</a>
+<a href="https://tinyurl.com/mrx5xb9z" target="_blank" class="p-3 bg-gray-100 text-center rounded hover:bg-gray-200">4번 링크</a>
+
       </div>
 
       <!-- 설명 박스 -->
