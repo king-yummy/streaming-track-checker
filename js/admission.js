9b9960503972b6a5f88f9906a93ab2bc195e55f7
king-yummy <wingting2642@naver.com>
2026-01-02 14:05:52 +0900

    광고 유형 변경

--- js/admission.js
+++ js/admission.js
@@ -370,13 +370,14 @@ document.addEventListener("DOMContentLoaded", () => {
           </h3>
           <div style="text-align: center">
             <ins
-              class="adsbygoogle"
-              style="display: block"
-              data-ad-client="ca-pub-9063401338616510"
-              data-ad-slot="3718792037"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            ></ins>
+  class="adsbygoogle"
+  style="display:block"
+  data-ad-format="fluid"
+  data-ad-layout-key="+2k+pl+16-1y+d2"
+  data-ad-client="ca-pub-9063401338616510"
+  data-ad-slot="1251777927"
+></ins>
+
           </div>
         </section>
       </div>
